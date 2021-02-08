d9952bdaa80029afb52d4abb883930f6a7e564c4
Albert Smit <talkto@albertsm.it>
2021-02-08 15:16:56 +0100

    Prettier file

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/prisma/prisma-client-go v0.4.0
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
 )
--- server.go
+++ server.go
@@ -1,71 +1,93 @@
 package main
 
 import (
+	"fmt"
 	"context"
 	"strconv"
 	"net/http"
 	"encoding/json"
 	"voter/prisma/db"
+	"golang.org/x/net/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	// Prisma 2
+func getRoot(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func getWS(c echo.Context) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		for {
+			// Write
+			err := websocket.Message.Send(ws, "Hello, Client!")
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
+			// Read
+			msg := ""
+			err = websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			fmt.Printf("%s\n", msg)
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
+
+func getPost(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, Post!")
+}
+
+func getSinglePost(c echo.Context) error {
 	client := db.NewClient()
-	if err := client.Prisma.Connect(); err != nil {
-			return
+	ctx := context.Background()
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	post, err := client.Post.FindUnique(
+		db.Post.ID.Equals(id),
+	).Exec(ctx)
+
+	if err != nil {
+			return err
 	}
 
-	defer func() {
-			if err := client.Prisma.Disconnect(); err != nil {
-					panic(err)
-			}
-	}()
+	result, _ := json.MarshalIndent(post, "", "  ")
+	return c.JSON(http.StatusOK, result)
+}
 
+func postNewPost(c echo.Context) error {
+	client := db.NewClient()
 	ctx := context.Background()
 
-	// Echo
+	createdPost, err := client.Post.CreateOne(
+		db.Post.Title.Set("Hi from Prisma!"),
+		db.Post.Published.Set(true),
+		db.Post.Desc.Set("Prisma is a database toolkit and makes databases easy."),
+	).Exec(ctx)
+
+	if err != nil {
+			return err
+	}
+
+	result, _ := json.MarshalIndent(createdPost, "", "  ")
+	return c.JSON(http.StatusOK, result)
+}
+
+func main() {
 	e := echo.New()
+
 	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-
-	e.GET("/post", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Post!")
-	})
-
-	e.GET("/post/:id", func(c echo.Context) error {
-		id, _ := strconv.Atoi(c.Param("id"))
-
-		post, err := client.Post.FindUnique(
-			db.Post.ID.Equals(id),
-		).Exec(ctx)
-
-		if err != nil {
-				return err
-		}
-
-		result, _ := json.MarshalIndent(post, "", "  ")
-		return c.JSON(http.StatusOK, result)
-	})
-
-	e.POST("/post", func(c echo.Context) error {
-		createdPost, err := client.Post.CreateOne(
-			db.Post.Title.Set("Hi from Prisma!"),
-			db.Post.Published.Set(true),
-			db.Post.Desc.Set("Prisma is a database toolkit and makes databases easy."),
-		).Exec(ctx)
-
-		if err != nil {
-				return err
-		}
-
-		result, _ := json.MarshalIndent(createdPost, "", "  ")
-		return c.JSON(http.StatusOK, result)
-	})
+	e.GET("/", getRoot)
+	e.GET("/ws", getWS)
+	e.GET("/post", getPost)
+	e.GET("/post/:id", getSinglePost)
+	e.POST("/post", postNewPost)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
