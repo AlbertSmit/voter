bdbe6a2d03137de471c4af0e89f6757c76a6a9a7
Albert Smit <talkto@albertsm.it>
2021-02-10 20:24:40 +0100

    Revert

--- server/main.go
+++ server/main.go
@@ -4,22 +4,45 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 
 	db "github.com/albertsmit/voter/server/prisma-client"
+	"github.com/gorilla/websocket"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-// CustomContext type
-type CustomContext struct {
-	echo.Context
-	Hub
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func getWS(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
 }
 
 func getSinglePost(c echo.Context) error {
@@ -82,7 +105,7 @@ func main() {
 		e.Static("/", "./web")
 	}
 
-	e.GET("/ws", serveWs)
+	e.GET("/ws", getWS)
 	e.POST("/post", postNewPost)
 	e.GET("/post/:id", getSinglePost)
 
