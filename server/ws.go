ac39062f591e9ea2ed09ed166d38b53674acd145
Albert Smit <talkto@albertsm.it>
2021-02-12 08:39:19 +0100

    Working "Chat" to iterate on.

--- server/ws.go
+++ server/ws.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/websocket"
 
 	echo "github.com/labstack/echo/v4"
@@ -30,12 +33,21 @@ import (
 // 	}
 // }
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		origin := r.Header.Get("Origin")
+    return origin == "http://localhost:8080" || origin == "https://votevotevotevote.herokuapp.com"
+	},
+}
 
 // WSHandler ...
 func (a *App) WSHandler(c echo.Context) error {
 	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
 
